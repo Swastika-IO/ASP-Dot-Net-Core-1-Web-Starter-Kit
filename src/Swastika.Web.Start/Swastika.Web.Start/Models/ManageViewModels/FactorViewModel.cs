72f644a51ef787569f75f3bd9a563948f3c83ee8
Smilefounder <huy.nguyen@smileway.org>
2017-07-06 18:36:14 +0700

    Init code base with basic AdminLTE

---
+++ src/Swastika.Web.Start/Swastika.Web.Start/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swastika.Web.Start.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
