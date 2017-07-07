9a36c1f40f906774efc1b5075845439ab9ae73cb
Smilefounder <i.love.to.smile.around@gmail.com>
2017-07-07 02:30:13 -0700

    Added AdminLTE basic components

---
+++ src/Swastika.Web.Start/Swastika.Web.Start/Areas/Portal/ViewComponents/MainSidebar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swastika.Web.Start.Areas.Portal.ViewComponents
+{
+    public class MainSidebar : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
